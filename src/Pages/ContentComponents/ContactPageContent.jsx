2eaf737313d92ae3e3ea2da1a6e7152e22cbced1
Yavuz7 <yavuzyurtseven1@gmail.com>
2023-04-21 18:56:42 -0400

    Added Functionality to Contact page!

--- src/Pages/ContentComponents/ContactPageContent.jsx
+++ src/Pages/ContentComponents/ContactPageContent.jsx
@@ -3,35 +3,54 @@ import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import '../ContentComponents/BaseContentStyleSheet.css'
+import { useState } from 'react';
 
-export default function ContactPageContent() {
+export default function ContactPageContent(event) {
     document.body.style.overflow='auto';
+
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      else{
+        alert('Form Submitted!');
+      }
+      setValidated(true);
+    };
     return(
     <>
     <Container className='ContentContainer'>
     <Card className='mt-5 text-start' style={{ width: '55vw', height: '65vmin' }}>    
       <Card.Body>
         <Card.Title className='fs-4' >Contact Me</Card.Title>
-        <Form>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Form.Group className='mb-2'>
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter name" />
+          <Form.Control required type="text" placeholder="Enter name" />
+          <Form.Control.Feedback type="invalid">Please enter a name!</Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className='mb-3'>
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control required type="email" placeholder="Enter email"/>
+          <Form.Control.Feedback type="invalid">Please enter a valid email!</Form.Control.Feedback>
         </Form.Group>
         
         <Form.Group className='mt-3'>
         <Form.Label>Your Message</Form.Label>
-          <Form.Control as="textarea" rows={4} placeholder='Enter Message'/>
+          <Form.Control required as="textarea" rows={4} placeholder='Enter Message'/>
+          <Form.Control.Feedback type="invalid">Please enter your message!</Form.Control.Feedback>
         </Form.Group>
 
-        </Form>
         <Button className='mt-3' variant="primary" type="submit">
         Send Message
       </Button>
+        </Form>
+
       </Card.Body>
     </Card>
     </Container>
