93de03f7e80c3bdf24ba7ce29916cea1f8ced7f9
Yavuz7 <yavuzyurtseven1@gmail.com>
2023-04-21 21:46:13 -0400

    Added and Formatted Logo and Banner

--- src/Pages/EveryPageStuff/MyFooter.jsx
+++ src/Pages/EveryPageStuff/MyFooter.jsx
@@ -1,15 +1,15 @@
 import "../EveryPageStuff/MyFooter.css"
+import smallLogo from "../EveryPageStuff/Images/Yaya_Banaya_Logo_v2small.png"
 
 export default function MyFooter() {
     return(
         <>
-        <footer>
+        <footer className="imgContainer">
+        <img className="smallLogo" alt="Logo" src={smallLogo}></img>
             <ul>
-                <img alt="Logo"></img>
                 <li>&#169; 2023 Yavuz Yurtseven</li>
                 <li>Terms Of Service</li>
-                <li>Privacy Notice</li>
-                
+                <li>Privacy Notice</li>              
             </ul>
         </footer>
         </>
--- src/Pages/EveryPageStuff/WebsiteHeader.jsx
+++ src/Pages/EveryPageStuff/WebsiteHeader.jsx
@@ -1,9 +1,10 @@
 import "../EveryPageStuff/WebsiteHeader.css"
+import bigLogo from "../EveryPageStuff/Images/Yaya_Banaya_Logo_v2.png"
 
 export default function WebsiteHeader(){
     return(
         <div className="headerBg">
-            <h2>Welcome To EggLand</h2>
+            <img alt="Main Logo" src={bigLogo}></img>
         </div>
     )
 }
