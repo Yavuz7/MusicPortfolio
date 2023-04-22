7bd5c3dfe8a5ee4391ba482f196b3e1f325e0048
Yavuz7 <yavuzyurtseven1@gmail.com>
2023-04-22 17:37:04 -0400

    Worked on css a bit

--- src/Pages/ContentComponents/HomePageFeaturedSong.jsx
+++ src/Pages/ContentComponents/HomePageFeaturedSong.jsx
@@ -9,7 +9,7 @@ export default function HomePageFeatureSong(){
         <>
         <Card className="mx-auto position-relative musicCardDiv ContentContainer" style={{ width: '55vw', height: '62vmin'}}>
             <Card.Title className="p-4">Crows In The Rain ft. Rare Antidote</Card.Title>
-            <Card.Body className="fs-3 text-start featuredCardBody mt-4">&emsp;&emsp; <u>Why is this song featured</u> <br/><br/> This is a collaborative piece between my friend and I, about something creepy I suppose. The mixing took a long time, but it came out well so I'm very proud to feature it!</Card.Body>
+            <Card.Body className="text-start featuredCardBody mt-4">&emsp;&emsp; <u>Why is this song featured</u> <br/><br/> This is a collaborative piece between my friend and I, about something creepy I suppose. The mixing took a long time, but it came out well so I'm very proud to feature it!</Card.Body>
             <Container className="musicPlayer featuredPlayer float px-0 py-5">
             <ReactPlayer width = "40%" url="https://soundcloud.com/yaya-banaya/crows-in-the-rain-ft-rare-antidote?in=yaya-banaya"/>
             </Container>
